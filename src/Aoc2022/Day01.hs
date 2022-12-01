aa788ac242efa054a0d6136487409c131a88328f
Brian Magnuson <bmagnuso@amazon.com>
2022-12-01 09:40:52 -0500

    day 1

--- src/Aoc2022/Day01.hs
+++ src/Aoc2022/Day01.hs
@@ -7,7 +7,13 @@ where
 import Aoc2022.AocUtils
 import Control.Applicative
 import Data.Attoparsec.Text qualified as P
+import Data.List (sortOn)
+import Data.Ord (Down (..))
 
-part1 = undefined
+parser = P.sepBy1 (many (P.decimal <* P.endOfLine)) P.endOfLine
 
-part2 = undefined
+input = getInput "input/day01.txt" parser
+
+part1 = maximum (map sum input)
+
+part2 = sum . take 3 . sortOn Down $ map sum input
