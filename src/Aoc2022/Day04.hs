0397ef3232bb346d6f1aa363352ee7f82ede1b14
Brian Magnuson <bmagnuso@amazon.com>
2022-12-04 10:38:57 -0500

    day 4

--- src/Aoc2022/Day04.hs
+++ src/Aoc2022/Day04.hs
@@ -8,6 +8,33 @@ import Aoc2022.AocUtils
 import Control.Applicative
 import Data.Attoparsec.Text qualified as P
 
-part1 = undefined
+data Range = Range Integer Integer deriving (Show)
 
-part2 = undefined
+data Pair = Pair Range Range deriving (Show)
+
+parser = many (pPair <* P.endOfLine)
+  where
+    pPair = do
+      p1 <- pRange
+      P.char ','
+      p2 <- pRange
+      return $ Pair p1 p2
+    pRange = do
+      l <- P.decimal
+      P.char '-'
+      r <- P.decimal
+      return $ Range l r
+
+input = getInput "input/day04.txt" parser
+
+contains :: Pair -> Bool
+contains (Pair (Range l1 r1) (Range l2 r2)) =
+  l1 >= l2 && r1 <= r2 || l2 >= l1 && r2 <= r1
+
+overlap :: Pair -> Bool
+overlap p@(Pair (Range l1 r1) (Range l2 r2)) =
+  contains p || (l1 <= l2 && r1 >= l2) || (l1 <= r2 && r1 >= r2)
+
+part1 = sum . map ((\x -> if x then 1 else 0) . contains) $ input
+
+part2 = sum . map ((\x -> if x then 1 else 0) . overlap) $ input
