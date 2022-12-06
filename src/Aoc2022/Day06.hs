1f6544d47211f979561d7982905d1e7ac45463ec
Brian Magnuson <bmagnuso@amazon.com>
2022-12-06 01:31:10 -0500

    day 6

--- src/Aoc2022/Day06.hs
+++ src/Aoc2022/Day06.hs
@@ -7,7 +7,16 @@ where
 import Aoc2022.AocUtils
 import Control.Applicative
 import Data.Attoparsec.Text qualified as P
+import Data.List (nub)
+import Data.Text (unpack)
 
-part1 = undefined
+input = getInput "input/day06.txt" (unpack <$> P.takeText)
 
-part2 = undefined
+marker :: Int -> [Char] -> Int
+marker z s = let (xs, ys) = splitAt z s in go (reverse xs) z ys
+  where
+    go k r (x : xs) = if length (nub k) == z then r else go (x : init k) (r + 1) xs
+
+part1 = marker 4 input
+
+part2 = marker 14 input
