33b69a67d5d909376da4b0dd4897cf150f61240c
Brian Magnuson <bmagnuso@amazon.com>
2022-12-07 23:15:44 -0500

    day 7

---
+++ app/FormatLeaderBoard.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module FormatLeaderBoard
+  (
+  )
+where
+
+import Control.Lens
+import Data.Aeson
+import Data.Aeson.TH
+import qualified Data.ByteString.Lazy as B
+import qualified Data.Map as M
+import Data.Time
+import Data.Time.Clock
+import Data.Time.Clock.POSIX
+import GHC.Generics
+import Text.Printf
+
+data StarTime = StarTime
+  { _get_star_ts :: Int,
+    _star_index :: Int
+  }
+  deriving (Show, Generic)
+
+makeLenses ''StarTime
+deriveJSON defaultOptions {fieldLabelModifier = drop 1} ''StarTime
+
+data Member = Member
+  { _local_score :: Int,
+    _global_score :: Int,
+    _id :: Int,
+    _completion_day_level :: M.Map Int (M.Map Int StarTime),
+    _last_star_ts :: Int,
+    _name :: Maybe String,
+    _stars :: Int
+  }
+  deriving (Show, Generic)
+
+makeLenses ''Member
+deriveJSON defaultOptions {fieldLabelModifier = drop 1} ''Member
+
+data LeaderBoard = LeaderBoard
+  { _event :: String,
+    _owner_id :: Int,
+    _members :: M.Map Int Member
+  }
+  deriving (Show, Generic)
+
+makeLenses ''LeaderBoard
+deriveJSON defaultOptions {fieldLabelModifier = drop 1} ''LeaderBoard
+
+solveTime :: Int -> Int -> String
+solveTime d f =
+  let s = UTCTime (fromGregorian 2022 12 d) (3600 * 5)
+      e = posixSecondsToUTCTime (fromIntegral f)
+   in formatTime defaultTimeLocale "%3h:%02M:%02S" (diffUTCTime e s)
+
+printBoard b = mapM_ printMember (b ^. members)
+  where
+    printMember m = do
+      case m ^. name of
+        Just m' -> printf "%s (%d)\n" m' (m ^. local_score)
+        Nothing -> printf "Anonymous user #%d\n" (m ^. FormatLeaderBoard.id)
+      mapM_ printDay [(d, getTs d 1, getTs d 2) | d <- [1 .. 25]]
+      putStrLn ""
+      where
+        getTs :: Int -> Int -> Maybe StarTime
+        getTs dd s = m ^? completion_day_level . ix dd . ix s
+        printDay = \case
+          (_, Nothing, Nothing) -> return ()
+          (d, Just t1, Nothing) -> printf "%d %s\n" d (solveTime d (t1 ^. get_star_ts))
+          (d, Just t1, Just t2) -> do
+            printf "%d %s, %s" d (solveTime d (t1 ^. get_star_ts)) (solveTime d (t2 ^. get_star_ts)) >> putStrLn ""
+
+main = do
+  b <- B.readFile "trial.txt"
+  case decode b :: Maybe LeaderBoard of
+    Nothing -> putStrLn "Failed to parse"
+    Just json -> printBoard json
--- src/Aoc2022/Day07.hs
+++ src/Aoc2022/Day07.hs
@@ -6,8 +6,119 @@ where
 
 import Aoc2022.AocUtils
 import Control.Applicative
+import Control.Comonad
+import Control.Comonad.Cofree
+import Control.Lens hiding ((:<))
+import Control.Monad.State.Strict
 import Data.Attoparsec.Text qualified as P
+import Data.Eq.Deriving
+import Data.List (sort)
+import Data.Map (Map)
+import Data.Map qualified as M
+import Data.Text (Text)
+import Text.Show.Deriving
 
-part1 = undefined
+data Cmd = Up | Down Text | Ls [TraceEntry] deriving (Show)
 
-part2 = undefined
+data TraceEntry
+  = TraceDir Text
+  | TraceFile Text Int
+  deriving (Show)
+
+data TreeF a = TreeF
+  { _dirs :: Map Text a,
+    _files :: Map Text Int
+  }
+  deriving (Functor, Show)
+
+type Tree a = Cofree TreeF a
+
+$(deriveShow1 ''TreeF)
+$(deriveEq1 ''TreeF)
+
+makeLenses ''TreeF
+
+type Crumb a = (Tree a, Text)
+
+type Breadcrumbs a = [Crumb a]
+
+type Zipper a = (Tree a, Breadcrumbs a)
+
+parser = do
+  P.string "$ cd /\n"
+  many (pCd <|> pLs)
+  where
+    pCd = do
+      P.string "$ cd "
+      (P.string "..\n" >> return Up) <|> (Down <$> pRest)
+    pLs = do
+      P.string "$ ls\n"
+      Ls <$> many (pDir <|> pFile)
+    pDir = TraceDir <$> (P.string "dir " *> pRest)
+    pFile = do
+      size <- P.decimal
+      P.char ' '
+      name <- pRest
+      return $ TraceFile name size
+    pRest = P.takeWhile (/= '\n') <* P.endOfLine
+
+input = getInput "input/day07.txt" parser
+
+enter :: Tree a -> Zipper a
+enter t = (t, [])
+
+exit :: Zipper a -> Tree a
+exit (t, _) = t
+
+down :: Text -> Zipper a -> Zipper a
+down n ((x@(_ :< TreeF d f), b)) = (d ^?! ix n, (x, n) : b)
+
+emptyDir = () :< (TreeF M.empty M.empty)
+
+up :: Zipper a -> Zipper a
+up (t, []) = (t, [])
+up (t, (a :< TreeF d f, n) : bs) = (a :< TreeF (d & at n .~ Just t) f, bs)
+
+toRoot :: Zipper a -> Zipper a
+toRoot z@(_, []) = z
+toRoot z = toRoot (up z)
+
+adjust f (a :< t, b) = (a :< f t, b)
+
+data St = St {_tree :: Zipper ()} deriving (Show)
+
+makeLenses ''St
+
+f :: Cmd -> State St ()
+f Up = tree %= up
+f (Down d) = tree %= down d
+f (Ls t) = mapM_ f t
+  where
+    f :: TraceEntry -> State St ()
+    f (TraceDir d) = tree %= adjust (dirs . at d .~ Just emptyDir)
+    f (TraceFile n s) = tree %= adjust (files . at n .~ Just s)
+
+mkTree =
+  let st = execState (traverse f input) (St (enter (() :< TreeF M.empty M.empty)))
+   in exit (toRoot (st ^. tree))
+
+sumFiles :: Tree () -> Tree Int
+sumFiles s = extend f s
+  where
+    f :: Tree () -> Int
+    f (() :< TreeF d f) =
+      let dsum = sum (map (extract . sumFiles) (M.elems d)) :: Int
+       in dsum + sum (M.elems f)
+
+collectSizes :: Tree Int -> Tree [Int]
+collectSizes s = let f (a :< TreeF d _) = a : (concat (map (extract . collectSizes) (M.elems d))) in extend f s
+
+annTree = sumFiles mkTree
+
+dirSizes = extract (collectSizes annTree)
+
+toFree = 30000000 - (70000000 - extract annTree)
+
+part1 = sum (filter (<= 100000) dirSizes)
+
+part2 = minimum (filter (>= toFree) dirSizes)
