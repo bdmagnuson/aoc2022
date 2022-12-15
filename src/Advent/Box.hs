e0ca75dd1e38e25ebae3a3ed477accdbc032321b
Brian Magnuson <bmagnuso@amazon.com>
2022-12-15 13:26:52 -0500

    day 15

---
+++ src/Advent/Box.hs
@@ -0,0 +1,131 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
+
+-- |
+-- Module      : Advent.Box
+-- Description : N-dimensional boxes
+-- Copyright   : 2021 Eric Mertens
+-- License     : ISC
+-- Maintainer  : emertens@gmail.com
+--
+-- This module expresses boxes as a list of bounds on each axis. This representation
+-- enables efficient intersection and subtraction operations.
+module Advent.Box where
+
+import Control.Monad (foldM)
+import Data.Kind (Type)
+import Data.List (foldl1')
+import GHC.Stack (HasCallStack)
+
+-- | Natural numbers (used for type index)
+data Nat
+  = -- | zero
+    Z
+  | -- | successor
+    S Nat
+
+-- | An n-dimensional box.
+data Box :: Nat -> Type where
+  Pt ::
+    -- | A single point
+    Box 'Z
+  Dim ::
+    -- | inclusive lower bound
+    !Int ->
+    -- | exclusive upper bound
+    !Int ->
+    -- | lower dimensional box
+    Box n ->
+    -- | A box extended along an axis
+    Box ('S n)
+
+deriving instance Show (Box n)
+
+deriving instance Eq (Box n)
+
+deriving instance Ord (Box n)
+
+-- | Returns the nmber of points contained in a box.
+--
+-- >>> size Pt -- 0D point
+-- 1
+--
+-- >>> size (Dim 1 4 Pt) -- 1D segment; length
+-- 3
+--
+-- >>> size (Dim 1 4 (Dim 0 3 Pt)) -- 2D rectangle; area
+-- 9
+--
+-- >>> size (Dim 1 4 (Dim 0 3 (Dim 0 2 Pt))) -- 3D cuboid; volume
+-- 18
+size :: Box n -> Int
+size Pt = 1
+size (Dim lo hi box) = (hi - lo) * size box
+
+-- | The intersection of two boxes is the intersection of their segments.
+--
+-- >>> intersectBox (Dim 0 2 (Dim 0 3 Pt)) (Dim 1 4 (Dim 2 4 Pt))
+-- Just (Dim 1 2 (Dim 2 3 Pt))
+intersectBox :: Box n -> Box n -> Maybe (Box n)
+intersectBox Pt Pt = Just Pt
+intersectBox (Dim a b xs) (Dim c d ys) =
+  [Dim x y zs | let x = max a c, let y = min b d, x < y, zs <- intersectBox xs ys]
+
+-- | Intersection of one or more boxes.
+intersectBoxes :: HasCallStack => [Box n] -> Maybe (Box n)
+intersectBoxes [] = error "intersectBoxes: empty intersection"
+intersectBoxes (x : xs) = foldM intersectBox x xs
+
+-- | Subtract the first box from the second box returning a list of boxes
+-- that cover all the remaining area.
+--
+-- >>> subtractBox (Dim 2 3 Pt) (Dim 0 4 Pt)
+-- [Dim 0 2 Pt,Dim 3 4 Pt]
+--
+-- >>> subtractBox (Dim 3 5 Pt) (Dim 0 4 Pt)
+-- [Dim 0 3 Pt]
+--
+-- >>> subtractBox (Dim 0 1 Pt) (Dim 1 2 Pt)
+-- [Dim 1 2 Pt]
+--
+-- >>> subtractBox (Dim 0 1 (Dim 0 1 Pt)) (Dim 0 2 (Dim 0 2 Pt))
+-- [Dim 1 2 (Dim 0 2 Pt),Dim 0 1 (Dim 1 2 Pt)]
+--
+-- >>> subtractBox (Dim 0 9 Pt) (Dim 3 6 Pt)
+-- []
+subtractBox ::
+  -- | remove this
+  Box n ->
+  -- | from this
+  Box n ->
+  -- | leaving these
+  [Box n]
+subtractBox b1 b2 =
+  case intersectBox b1 b2 of
+    Nothing -> [b2]
+    Just b -> subtractBox' b b2
+
+-- | Worker for 'subtractBox' where the first argument is a
+-- subset of the second argument.
+subtractBox' :: Box n -> Box n -> [Box n]
+subtractBox' Pt Pt = []
+subtractBox' (Dim a b xs) (Dim c d ys) =
+  [Dim c a ys | c < a]
+    ++ [Dim b d ys | b < d]
+    ++ [Dim a b zs | zs <- subtractBox' xs ys]
+
+-- | Compute the box that encompasses both arguments.
+unionBox :: Box n -> Box n -> Box n
+unionBox (Dim a b x) (Dim c d y) = Dim (min a c) (max b d) (unionBox x y)
+unionBox Pt Pt = Pt
+
+-- | Compute the box that encompasses all of the boxes in the list.
+unionBoxes :: [Box n] -> Box n
+unionBoxes = foldl1' unionBox
--- src/Aoc2022/Day15.hs
+++ src/Aoc2022/Day15.hs
@@ -4,10 +4,82 @@ module Aoc2022.Day15
   )
 where
 
+import Advent.Box
 import Aoc2022.AocUtils
 import Control.Applicative
 import Data.Attoparsec.Text qualified as P
+import Data.List (nub, sortBy)
+import Data.Maybe (catMaybes)
 
-part1 = undefined
+type Coord = (Int, Int)
 
-part2 = undefined
+data Sensor = Sensor Coord Int deriving (Show)
+
+parser = P.sepBy1 pPair P.endOfLine
+  where
+    loc = P.signed P.decimal
+    pPair = do
+      P.string "Sensor at x="
+      sx <- loc
+      P.string ", y="
+      sy <- loc
+      P.string ": closest beacon is at x="
+      bx <- loc
+      P.string ", y="
+      by <- loc
+      return $ (Sensor (sx, sy) (abs (sx - bx) + abs (sy - by)), (bx, by))
+
+(diamonds, beacons) = unzip $ getInput "input/day15.txt" parser
+
+noBeaconRow :: Int -> Sensor -> Maybe (Int, Int)
+noBeaconRow row (Sensor (sx, sy) d) =
+  let wing = d - (abs (sy - row))
+   in if wing < 0 then Nothing else Just (sx - wing, sx + wing)
+
+row = 2000000
+
+part1 =
+  let rngs = merge (sortBy g (catMaybes (map (noBeaconRow row) diamonds)))
+      total = foldl (\acc (l, r) -> acc + (r - l + 1)) 0 rngs
+      beaconRow = nub $ map fst $ filter (\(_, y) -> y == row) beacons
+      sub = sum . (map (\x -> if x then 1 else 0)) $ map (\x -> any (\(l, r) -> x >= l || x <= r) rngs) beaconRow
+   in total - sub
+  where
+    g (l1, r1) (l2, r2) =
+      case compare l1 l2 of
+        LT -> LT
+        GT -> GT
+        EQ -> compare r1 r2
+    merge :: [(Int, Int)] -> [(Int, Int)]
+    merge [] = []
+    merge (x : []) = [x]
+    merge ((l1, r1) : rest@((l2, r2) : xs)) = if l2 <= r1 + 1 then merge ((l1, max r1 r2) : xs) else (l1, r1) : (merge rest)
+
+-- Make diamonds into rectangles with a change of basis.  Then we can subtract rectangles to get the final location and convert back to 'diamond' cooridinates.
+
+cb (x, y) = (x + y, -x + y)
+
+icb (x, y) = ((x - y) `div` 2, (x + y) `div` 2)
+
+diamond2rect :: Sensor -> Box ('S ('S 'Z))
+diamond2rect (Sensor (sx, sy) d) =
+  let (x1, y1) = cb (sx - d, sy)
+      (x2, y2) = cb (sx + d, sy)
+   in Dim (min x1 x2) ((max x1 x2) + 1) (Dim (min y1 y2) ((max y1 y2) + 1) Pt)
+
+rect2diamond :: Box ('S ('S 'Z)) -> ((Int, Int), (Int, Int))
+rect2diamond (Dim x1 x2 (Dim y1 y2 Pt)) = (icb (x1, y1), icb (x2 - 1, y2 - 1))
+
+subtractAllOf :: [Box n] -> Box n -> [Box n]
+subtractAllOf xs ys = foldl remove1 [ys] xs
+  where
+    remove1 acc x = concatMap (subtractBox x) acc
+
+search = 4000000
+
+part2 =
+  let start = diamond2rect (Sensor ((search `div` 2), (search `div` 2)) (search))
+      left = map rect2diamond (subtractAllOf (map diamond2rect diamonds) start)
+      -- Only one box should remains that doesn't have a corner outside of the search space so just filter out anthing outside the space.
+      [(x, y)] = filter (\(x, y) -> x >= 0 && x <= search && y >= 0 && y <= search) (map fst left)
+   in 4000000 * x + y
